85852dc2609eb1a4893f9c0b39c879551992e192
kloss <tkloss@web.de>
2015-04-30 08:18:34 +0200

    Add Response and Request, implement Request

---
+++ src/main/java/de/adesso/tokl/webserver/sockets/Response.java
@@ -0,0 +1,13 @@
+package de.adesso.tokl.webserver.sockets;
+
+import java.io.OutputStream;
+
+/**
+ * Created by kloss on 30.04.2015.
+ */
+public class Response {
+
+    public Response(OutputStream output) {
+
+    }
+}
