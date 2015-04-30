a88bfc81636fead67e4bd62ce01080e0f6939b09
kloss <tkloss@web.de>
2015-04-30 08:59:11 +0200

    Refactorings

--- src/main/java/de/adesso/tokl/webserver/sockets/Response.java
+++ src/main/java/de/adesso/tokl/webserver/sockets/Response.java
@@ -7,16 +7,22 @@ import java.io.OutputStream;
 
 /**
  * Created by kloss on 30.04.2015.
+ * <p>
+ * Represents a HTML reponse to a given HTML request.
  */
 public class Response {
 
-    private static final int BUFFER_SIZE = 8096;
+    private static final String ERROR_MESSAGE = "HTTP/1.1 404 File Not Found\r\n" +
+            "Content-Type: text/html\r\n" +
+            "Content-Length: 23\r\n" +
+            "\r\n" +
+            "<h1>File Not Found</h1>";
+    private static final int BUFFER_SIZE = 2048;
     private Request request;
     private OutputStream output;
     private String rootDirectory;
 
     public Response(OutputStream output, String rootDirectory) {
-
         this.output = output;
         this.rootDirectory = rootDirectory;
     }
@@ -25,13 +31,19 @@ public class Response {
         this.request = request;
     }
 
+    /**
+     * Checks wheter or not the file exists and then either returns an error message or the file
+     *
+     * @throws IOException if the file object can not be instantiated
+     */
     public void sendStaticResource() throws IOException {
-        byte[] bytes = new byte[BUFFER_SIZE];
+
         FileInputStream fis = null;
         try {
-            File file = new File(rootDirectory , request.getUri());
+            File file = new File(rootDirectory, request.getUri());
             if (file.exists()) {
                 fis = new FileInputStream(file);
+                byte[] bytes = new byte[BUFFER_SIZE];
                 int ch = fis.read(bytes, 0, BUFFER_SIZE);
 
                 while (ch != -1) {
@@ -39,20 +51,14 @@ public class Response {
                     ch = fis.read(bytes, 0, BUFFER_SIZE);
                 }
             } else {
-                // file not found
-                String errorMessage = "HTTP/1.1 404 File Not Found\r\n" +
-                        "Content-Type: text/html\r\n" +
-                        "Content-Length: 23\r\n" +
-                        "\r\n" +
-                        "<h1>File Not Found</h1>";
-                output.write(errorMessage.getBytes());
+                output.write(ERROR_MESSAGE.getBytes());
             }
-        } catch (Exception e) {
-            // thrown if cannot instantiate a File object
-            System.out.println(e.toString());
+        } catch (IOException ioException) {
+            System.out.println(ioException.toString());
         } finally {
             if (fis != null) fis.close();
         }
     }
+
 
 }
