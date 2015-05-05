b20d0de36cb8af984e7915ce8bf3501a1e1ecae2
kloss <tobias.kloss@web.de>
2015-05-05 16:35:47 +0200

    Finish Configurable

---
+++ src/main/java/de/adesso/tokl/webserver/ServerLauncher.java
@@ -0,0 +1,48 @@
+package de.adesso.tokl.webserver;
+
+import de.adesso.tokl.webserver.sockets.ServerConfiguration;
+import de.adesso.tokl.webserver.sockets.SocketsWebServer;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Created by kloss on 30.04.2015.
+ * <p>
+ * Startup class to initialise the Server
+ */
+class ServerLauncher {
+
+    public static void main(String[] args) {
+        new ServerLauncher().launch();
+    }
+
+    public void launch(){
+
+        ServerConfiguration config = null;
+
+        try {
+            checkAndCreateUserPropertiesFile();
+            config = new ServerConfiguration();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        WebServer server = new SocketsWebServer(config);
+        server.await();
+
+    }
+
+    private void checkAndCreateUserPropertiesFile() throws IOException {
+        File propertiesFile = new File(ServerConfiguration.USER_PROPERTIES_PATH);
+        if (!propertiesFile.exists()) {
+            InputStream in = getClass().getClassLoader().getResourceAsStream("serverConfig.properties");
+            Files.copy(in, Paths.get(propertiesFile.getAbsolutePath()));
+        }
+    }
+
+}
--- src/main/java/de/adesso/tokl/webserver/sockets/ServerConfiguration.java
+++ src/main/java/de/adesso/tokl/webserver/sockets/ServerConfiguration.java
@@ -1,9 +1,6 @@
 package de.adesso.tokl.webserver.sockets;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Properties;
 
 /**
@@ -13,24 +10,43 @@ import java.util.Properties;
  */
 public class ServerConfiguration {
 
-    private static final String DEFAULT_ROOT_DIR = System.getProperty("user.home") + File.separator + "SimpleWebserver";
-
+    public static final String USER_PROPERTIES_PATH = System.getProperty("user.home") + File.separator + "SimpleWebserver" + File.separator + "serverConfig.properties";
+    private static final String DEFAULT_ROOT_DIR = System.getProperty("user.home") + File.separator + "SimpleWebserver" + File.separator + "rootDir";
     Properties properties = new Properties();
 
-    public ServerConfiguration(String pathToPropertiesFile) throws IOException {
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(pathToPropertiesFile);
+    public ServerConfiguration() throws IOException {
+
+        InputStream inputStream = getInputStream();
+
         if (inputStream != null) {
             properties.load(inputStream);
         } else {
-            throw new FileNotFoundException("The givec properties file could not be found");
+            throw new FileNotFoundException("The given properties file could not be found");
         }
+
+        createRootDir();
+    }
+
+    private void createRootDir() {
+        File rootDir = new File(DEFAULT_ROOT_DIR);
+        rootDir.mkdirs();
+    }
+
+    private InputStream getInputStream() throws FileNotFoundException {
+        File propertiesFile = new File(USER_PROPERTIES_PATH);
+        InputStream inputStream;
+
+
+        inputStream = new FileInputStream(propertiesFile);
+
+        return inputStream;
     }
 
     public int getServerPort() {
         return Integer.parseInt(properties.getProperty("port", "8080"));
     }
 
-    public String getRootDirectory(){
-           return properties.getProperty("rootDirectory", DEFAULT_ROOT_DIR);
+    public String getRootDirectory() {
+        return properties.getProperty("rootDirectory", DEFAULT_ROOT_DIR);
     }
 }
